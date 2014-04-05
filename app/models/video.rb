93a9f80411b1c5e36d860776445655e3aadd2f21
remain <ponde.ponde.ponde@gmail.com>
2014-04-06 03:34:27 +0900

    add validation to title

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,11 @@
 class Video < ActiveRecord::Base
   acts_as_paranoid
-	validates :url,
-	  presence: true,
-		uniqueness: true
-	validates :thumbnail,
-		presence: true
+
+  validates :url,
+    presence: true,
+    uniqueness: true
+  validates :thumbnail,
+    presence: true
+  validates :title,
+    presence: true
 end
