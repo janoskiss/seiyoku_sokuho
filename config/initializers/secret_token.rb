de55aa6f04c3bd4d48276801eefbf58251154372
remain <ponde.ponde.ponde@gmail.com>
2014-04-05 23:23:58 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pvideos::Application.config.secret_key_base = 'f851a256b4d22ae3a42465e7b4b4ba076ce967d77093f2bf53c00838a2ec03f4306729750ef53c7be44ff805eaf4530a5b3f451be1e5b78c0e5e6bce2285344e'
