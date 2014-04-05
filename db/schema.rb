43b103bc4548737bcd377e18e18f087ef4c806a4
Miura Eiji <guitar.pg.sc@gmail.com>
2014-04-06 03:26:23 +0900

    rails g migration AddTitleToVideo title:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140405151905) do
+ActiveRecord::Schema.define(version: 20140405182335) do
 
   create_table "videos", force: true do |t|
     t.string   "url"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140405151905) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "thumbnail"
+    t.string   "title"
   end
 
 end
