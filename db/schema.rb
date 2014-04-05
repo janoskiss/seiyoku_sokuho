eb94364abc2517fd4eb8c815fe8f2a0ed1bcb1cf
Miura Eiji <guitar.pg.sc@gmail.com>
2014-04-06 01:26:38 +0900

    update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140405144922) do
+ActiveRecord::Schema.define(version: 20140405151905) do
 
   create_table "videos", force: true do |t|
     t.string   "url"
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "thumbnail"
   end
 
 end
