c7adf78f6db7ce1befccc3ed58c4ad235cfe5b10
remain <ponde.ponde.ponde@gmail.com>
2014-04-16 01:15:38 +0900

    :put_litter_in_its_place:

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,14 +1,5 @@
 class VideosController < ApplicationController
-  before_action :set_video, only: [:show, :edit, :update, :destroy]
-
-  # GET /videos
-  # GET /videos.json
   def index
     @videos = Video.scoped.page params[:page]
-  end
-
-  # GET /videos/1
-  # GET /videos/1.json
-  def show
   end
 end
