8c27d08e923cf5ff69f3836b756581f8050faf9d
remain <ponde.ponde.ponde@gmail.com>
2014-04-17 01:26:03 +0900

    adding default value and sanitization

--- db/migrate/20140410155421_add_next_check_deletion_at_to_videos.rb
+++ db/migrate/20140410155421_add_next_check_deletion_at_to_videos.rb
@@ -1,5 +1,16 @@
 class AddNextCheckDeletionAtToVideos < ActiveRecord::Migration
-  def change
+  def up
     add_column :videos, :next_check_deletion_at, :datetime
+
+    execute("select id from videos").each do |video|
+      next_check_deletion_at = Time.now + 24.hours + rand(24.hours)
+
+      sql = ["update videos set next_check_deletion_at = ? where id = ?", next_check_deletion_at, video["id"]]
+      execute(ActiveRecord::Base.__send__(:sanitize_sql_array, sql))
+    end
+  end
+
+  def down
+    remove_column :videos, :next_check_deletion_at
   end
 end
