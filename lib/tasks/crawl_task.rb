6001368d697fc186abad228ba60db47376031839
remain <ponde.ponde.ponde@gmail.com>
2014-04-18 03:08:28 +0900

    enable to work

--- lib/tasks/crawl_task.rb
+++ lib/tasks/crawl_task.rb
@@ -9,13 +9,21 @@ class Tasks::CrawlTask
     endpoint = URI.join(Video::BASE_URL, CRAWL_PATH)
     context  = Nokogiri.HTML(open(endpoint))
 
-    context.css("#content .mozaique .thumbBlock .thumbInside").each do |video|
-      path = video.css(".thumb a").attribute("href").text
+    _css = "#content .mozaique .thumbBlock .thumbInside"
+    # To use map!, calling to_a
+    thumb_insides = context.css(_css).to_a
+    thumb_insides.map! do |thumb_inside|
+      script = thumb_inside.css("script")
+      script.present? ? Nokogiri.parse(script.to_html) : thumb_inside
+    end
+
+    thumb_insides.each do |thumb_inside|
+      path = thumb_inside.css(".thumb a").attribute("href").text
 
       Video.create(
         code:      path.slice(/(?<=\/video)\d+/),
-        thumbnail: video.css(".thumb a").children.attribute("src").text,
-        title:     video.css("p a").children.text,
+        thumbnail: thumb_inside.css(".thumb a").children.attribute("src").text,
+        title:     thumb_inside.css("p a").children.text,
         next_deletion_detection_at: Video.next_deletion_detection_at(Time.now)
       )
     end
